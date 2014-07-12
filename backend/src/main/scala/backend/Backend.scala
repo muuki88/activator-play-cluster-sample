ac7cc4a6ff77b1572cc989337c93c84d24a26a9c
Nepomuk Seiler <nepomuk.seiler@mukis.de>
2014-07-12 09:52:05 +0200

    Changing Properties to scala map

--- backend/src/main/scala/backend/Backend.scala
+++ backend/src/main/scala/backend/Backend.scala
@@ -3,6 +3,7 @@ package backend
 import akka.actor._
 import com.typesafe.config.ConfigFactory
 import backend.factorial._
+import scala.collection.JavaConversions._
 
 /**
  * Booting a cluster backend node with all actors
@@ -17,9 +18,11 @@ object Backend extends App {
   }
 
   // System initialization
-  val properties = new java.util.Properties
-  properties setProperty ("akka.remote.netty.tcp.port", port)
-  val system = ActorSystem("application", (ConfigFactory parseProperties properties)
+  val properties = Map(
+      "akka.remote.netty.tcp.port" -> port
+  )
+  
+  val system = ActorSystem("application", (ConfigFactory parseMap properties)
     .withFallback(ConfigFactory.load())
   )
   
